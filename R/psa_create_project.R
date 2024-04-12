0cbea33fac4a7ae61a96532ac9b18e381e344444
debruine <debruine@gmail.com>
2024-04-12 11:15:30 +0100

    tests and name checking

--- R/psa_create_project.R
+++ R/psa_create_project.R
@@ -10,6 +10,13 @@
 psa_create_project <- function(path = "./",
                                psa_name = "PSA000-demo",
                                organisation = "psysciacc") {
+  usethis::ui_todo("Checking...")
+  # check name formatting ----
+  name_ok <- grepl("^[A-Z]{3,6}[0-9]{3}-[a-z0-9]{1,15}$", psa_name)
+  if (!name_ok) {
+    stop("PSA-style names are usually in a format like 'PSA001-faces', with the project code made of uppercase letters and 3 numbers, a dash, and then a short name (1 to 15 characters) made of only lowercase letters and numbers")
+  }
+
   # clone project locally ----
   usethis::ui_todo("Setting up project...")
   template_dir <- system.file("template", package = "psaverse")
--- tests/testthat/test-create_project.R
+++ tests/testthat/test-create_project.R
@@ -1,6 +1,40 @@
-test_that("defaults", {
-  tdir <- tempdir()
-  psa_create_project(tdir)
-  existing_files <- list.files(tdir)
-  expect_true("PSA000-demo" %in% existing_files)
+# setup
+tdir <- file.path(tempdir(), "PSA")
+if (dir.exists(tdir)) unlink(tdir, recursive = TRUE)
+dir.create(tdir, showWarnings = FALSE)
+default_dir <- file.path(tdir, "PSA000-demo")
+
+test_that("errors", {
+  expect_error(psa_create_project(tdir, "P001-faces"))
+  expect_error(psa_create_project(tdir, "PSALONGNAME001-faces"))
+  expect_error(psa_create_project(tdir, "psa001-faces"))
+  expect_error(psa_create_project(tdir, "PSA001-FACES"))
+  expect_error(psa_create_project(tdir, "PSA001-face-perception"))
+  expect_error(psa_create_project(tdir, "PSA001-faceperceptionmodels"))
 })
+
+test_that("defaults", {
+  # make default project
+  psa_create_project(tdir)
+
+  # check if default directory exists
+  existing_files <- list.files(tdir)
+  expect_true(dir.exists(default_dir))
+
+  # check expected directory structure
+  expected_subfiles <- c("DESCRIPTION")
+  subfiles <- list.files(default_dir, recursive = TRUE)
+  expect_equal(subfiles, expected_subfiles)
+
+  # check file customisations
+  desc <- readLines(file.path(default_dir, "DESCRIPTION"))
+  # expect_equal(desc[[1]], "Package: PSA000")
+  # expect_equal(desc[[2]], "Title: Data Archive for PSA000")
+  # expect_equal(desc[[8]], "License: CC BY 4.0")
+
+  # clean up
+  unlink(default_dir, recursive = TRUE)
+})
+
+# clean up
+unlink(tdir, recursive = TRUE)
