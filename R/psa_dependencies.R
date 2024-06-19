e3ec7075402ac8340e3de417ac3234811845e9fe
The Doom Lab <buchananlab@gmail.com>
2024-06-18 22:29:29 -0400

    adding dependencies function

--- R/psa_dependencies.R
+++ R/psa_dependencies.R
@@ -1,21 +1,29 @@
-#' Create PSA Project Description
+#' Add R package dependencies for analysis code
 #'
-#' @param path path to directory where the local project will be
-#' @param psa_name name of the repo (e.g., PSA000-demo)
+#' This function will look at all .R or .Rmd files for
+#' R packages to include in the package dependencies list.
+#' You can tell it to scan a specific folder - or use the
+#' default `inst` - or add a package directly. If you
+#' use the `add_package` option, the `scan_folder` will
+#' be ignored.
+#'
+#' @param scan_folder which folder should be examined for .R or .Rmd
+#'  files to find package libraries
+#' @param add_package Add a single package directly
 #'
 #' @import usethis
-#' @keywords create description
+#' @keywords dependencies
 #'
 #' @return NULL
 #' @export
 #' @example
-#' #
+#' # psa_dependencies(scan_folder = "inst")
 psa_dependencies <- function(
     scan_folder = "inst",
     add_package = NULL # has to be one at a time
     ) {
 
-  if (!is.null()){
+  if (!is.null(add_package)){
 
     use_package(add_package)
 
@@ -25,6 +33,7 @@ psa_dependencies <- function(
       path = scan_folder, # all folders in this project's inst
       recursive = TRUE,
       include.dirs = TRUE,
+      full.names = TRUE,
       pattern = "*.R|*.Rmd"
     )
 
@@ -32,6 +41,7 @@ psa_dependencies <- function(
       temp <- readLines(all.files[i])
       temp <- temp[grepl("library\\(.*\\)", temp)]
       temp <- gsub("library\\(|\\)", "", temp)
+      temp <- trimws(temp)
 
       sapply(temp, use_package)
     }
